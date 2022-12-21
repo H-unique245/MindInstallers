0a56b34f260061838952b39cb5c65033aad03e1f
pravin0428 <mohite46199@gmail.com>
2022-12-21 23:52:28 +0530

    homepage skelaton added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from "../Component/Navabar";
+import Footer from "../Component/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Footer/>
+    </ChakraProvider>
+  );
 }
