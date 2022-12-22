7bb4bfbe8e1425ef5932e7a715eaa6c75296aa21
kedar05121998 <101327577+kedar05121998@users.noreply.github.com>
2022-12-22 14:32:42 +0530

    teacher navbar section done

---
+++ pages/teacher/teach.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../../Component/Teacher/Nav'
+import StudentList from '../../Component/Teacher/StudentList'
+
+const Teach = () => {
+  return (
+    <div>
+        <Nav><StudentList/></Nav>
+    </div>
+  )
+}
+
+export default Teach
